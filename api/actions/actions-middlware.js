89ce5b61a51c6b1deb2893ffe533d40bba4651a2
Tafiqul <demianviotto@gmail.com>
2022-03-11 02:41:17 -0500

    finshed action. working on questions

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,29 @@
-// add middlewares here related to actions
+const Action = require('./actions-model');
+
+const verifyId = (req, res, next) => {
+  Action.get(req.params.id)
+    .then(action => {
+      if (!action) {
+        res.status(404).json({ message: `Unable to find Action with the given id ${req.params.id}`});
+      } else {
+          req.action = action;
+          next();
+      }
+    })
+    .catch(err => console.log(err))
+};
+
+const verifyPayload = (req, res, next) => {
+  const { project_id, description, notes } = req.body;
+
+  if (!project_id || !notes || !notes.trim()|| !description || !description.trim()) {
+    res.status(400).json({ message: 'Project_id, notes and description is required' });
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  verifyId,
+  verifyPayload
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,3 +1,45 @@
 const router = require('express').Router();
+const Action = require('./actions-model');
+const { verifyId, verifyPayload } = require('./actions-middlware');
+
+router.get('/', (req, res, next) => {
+  Action.get()
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(next)
+});
+
+router.get('/:id', verifyId, (req, res, next) => {
+  res.json(req.action);
+});
+
+router.post('/', verifyPayload, (req, res, next) => {
+  Action.insert(req.body)
+    .then(newAction => {
+      res.json(newAction);
+    })
+    .catch(next)
+});
+
+router.put('/:id', verifyId, verifyPayload, (req, res, next) => {
+  Action.update(req.params.id, req.body)
+    .then(updatedAction => {
+      res.json(updatedAction);
+    })
+    .catch(next)
+});
+
+router.delete('/:id', verifyId, (req, res, next) => {
+  Action.remove(req.params.id)
+    .then(deletedAction => {
+      res.json(deletedAction);
+    })
+    .catch(next)
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
 
 module.exports = router;
